a01143c1f4bc50c8dc7b88778dfb9f1fe385e2a9
AleksTheAraneus <perz.aleksandra@gmail.com>
2014-04-27 13:53:41 +0200

    first submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Thank you ever so much for taking your time to check my assignment (of many others
+## which may be more worthy of your attention (dramatism))!
+## Note that my explanations may be overly detailed and and naive - yes, I am new
+## to R and programming in general :)
 
-## Write a short comment describing this function
+## The below script is used for calculating and caching the inverse of a matrix.
+
+## The function makeCacheMatrix() takes a matrix as an argument and returns a list
+## of handy functions. Importantly, it allows for storage (caching) a calculated
+## inverse of the matrix with the x$setinv() function. It works analogously to the 
+## function makeVector() from the Assignment 2 description from the Coursera course
+## 'R programming" and, to be honest, doesn't differ much from it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setinv <- function(inv) {
+        i <<- inv
+    }
+    getinv <- function() {
+        i
+    }
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve() takes a product of makeCacheMatrix() as an argument
+## and returns an inverse of the matrix if it is possible. It checks if the result
+## has already been calculated; in this case it uses the cached data. Otherwise,
+## it calculates the inverse from scratch, calls x$setinv() from makeCacheMatrix()
+## to store it, and then returns the result. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
